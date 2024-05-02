518d1f8bd022cc8692d605359336ac366e9f28e6
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-05-02 11:02:32 +0000

    [Update]いいね機能の非同期通信化

--- app/controllers/comment_favorites_controller.rb
+++ app/controllers/comment_favorites_controller.rb
@@ -1,15 +1,17 @@
 class CommentFavoritesController < ApplicationController
   def create
-    post_comment = PostComment.find(params[:post_comment_id])
-    comment_favorite = current_user.comment_favorites.new(post_comment_id: post_comment.id)
+    @recommend_place_post = RecommendPlacePost.find(params[:recommend_place_post_id])
+    @post_comment = PostComment.find(params[:post_comment_id])
+    @post_comment.recommend_place_post_id = @recommend_place_post.id
+    comment_favorite = current_user.comment_favorites.new(post_comment_id: @post_comment.id)
     comment_favorite.save
-    redirect_to request.referer
   end
   
   def destroy
-    post_comment = PostComment.find(params[:post_comment_id])
-    comment_favorite = current_user.comment_favorites.find_by(post_comment_id: post_comment.id)
+    @recommend_place_post = RecommendPlacePost.find(params[:recommend_place_post_id])
+    @post_comment = PostComment.find(params[:post_comment_id])
+    @post_comment.recommend_place_post_id = @recommend_place_post.id
+    comment_favorite = current_user.comment_favorites.find_by(post_comment_id: @post_comment.id)
     comment_favorite.destroy
-    redirect_to request.referer
   end
 end
